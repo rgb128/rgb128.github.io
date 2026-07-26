70c4306005d5b1ee34765dc846823a30eea43eae
Alex Pascal <futupas@gmail.com>
2026-07-26 22:22:07 +0200

    add: divide'n'unite

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@
 
 const PIECES = [
     {
+        name: 'divide\'n\'unite',
+        description: 'divide and multiply',
+        link: 'https://rgb128.github.io/divide-n-unite',
+        imageSrc: 'images/divide-n-unite.png',
+        backgroundColor: '#ff1493',
+    },
+    {
         name: 'window',
         description: 'look what is outside the window',
         link: 'https://rgb128.github.io/window',
